85f78fbb53366e2171e43fa7af25ff962b55ca59
daveonduty006 <95148064+daveonduty006@users.noreply.github.com>
2022-04-22 20:46:02 -0400

    stuck at getting list of objects value

--- david_labo5b.py
+++ david_labo5b.py
@@ -1,94 +1,3 @@
-"""
-Laboratoire 5:
-Vous devez implémenter un système de facturation pour un garage automobile. Le système 
-de facturation doit avoir la liste des véhicules et les réparations à faire.
-
-Étape 1:
-Créer le constructeur de la classe automobile. Votre automobile doit avoir les 
-informations suivantes: Modèle, marque, année et couleur.
-
-Étape 2:
-Définir des méthodes permettant de retourner sous forme de string toutes les 
-informations par rapport à l'automobile.
-
-Étape 3:
-Créer le constructeur de la classe réparations. Votre objet réparation contenir 
-l'information par rapport au nom de la réparation, à son coût, au nombre d'heures de 
-travail et si la travail a été faite.
-
-Étape 4:
-Définir une méthode permettant de retourner sous forme de string toutes les 
-informations par rapport à la réparation.
-
-Étape 5 :
-Définir une classe système de facturation et l'instancier avec les 5 informations 
-suivantes:
-    Nissan Altima 2012 Bleu, Freins 800$ 6h non-fait
-    Ford Taurus 2000 Vert, BieletteAv-G 200$ 4h fait
-    Honda Civic 2020 Noir, Pare-Brise 1200$ 2h non-fait
-    Ford Escape 2011 Gris, Démarreur 800$ 5h non-fait
-    Toyota Corolla 2002 Bleu, Installation de pneus 80$ 1h fait
-
-Étape 6:
-Définir des méthodes permettant, d'ajouter des réparations, d'enlever des réparations, 
-d'avoir le nombre total d'heures de réparations faites, le revenu total des 
-réparations faites, le revenu horaire moyen, le nombre d'heures total de réparations 
-non faites, et le revenu total manqué des réparations non faites.
-
-Étape 7:
-Définir un menu permettant à un utilisateur de choisir une des options suivantes:
-    ajouter ou enlever une réparation(en incluant les véhicules associés)
-    voir la liste de réparations(en incluant les véhicules associés)
-    voir le nombre total d'heures de réparations faites
-    voir le revenu total de réparation faites
-    voir le nombre d'heures total de réparations non-faites
-    voir le revenu total manqué des réparations non-faites.
-"""
-
-"""
-classe car
-    attributs
-        maker
-        model
-        origin
-        color
-    méthodes
-        get_maker
-        get_model
-        get_origin
-        get_color
-
-classe reparation(car)
-    attributs
-        job_name
-        job_cost
-        job_length
-        job_state
-    méthodes
-        get_job_name
-        get_job_cost
-        get_job_length
-        get_job_state
-
-classe facturation(reparation)
-    méthodes
-        add_job
-        remove_job
-        get_done_jobs_length
-        get_done_jobs_cost
-        get_pay_rate
-        get_unfinished_jobs_length
-        get_unfinished_jobs_cost
-
-emp_menu():
-    ajouter ou enlever une réparation(en incluant les véhicules associés)
-    voir la liste de réparations(en incluant les véhicules associés)
-    voir le nombre total d'heures de réparations faites
-    voir le revenu total de réparation faites
-    voir le nombre d'heures total de réparations non-faites
-    voir le revenu total manqué des réparations non-faites.   
-"""
-
 class Car:
 
     def __init__(self, maker:str, model:str, year:int, color:str):
@@ -102,14 +11,14 @@ class Car:
            
 class Repair:
 
-    def __init__(self, job_name, job_cost, job_length, job_state): 
+    def __init__(self, job_name:str, job_cost:float, job_length:float, job_state:str): 
         self.job_name = job_name
         self.job_cost = job_cost
         self.job_length = job_length
         self.job_state = job_state
 
     def __str__(self):
-        return f"{self.job_name} {self.job_cost} {self.job_length} {self.job_state}"
+        return f"{self.job_name} {self.job_cost}$ {self.job_length}h {self.job_state}"
 
 class Billing:
         
@@ -118,24 +27,49 @@ class Billing:
         self.jobs = jobs
 
     def add_job(self):
-        pass
+        self.display()
+        print()
+        car_maker= input("Entrez le constructeur du véhicule: ")
+        car_model = input("Entrez le modèle: ")
+        car_year = int(input("Entrez l'année: "))
+        car_color = input("Entrez la couleur: ")
+        job_name = input("Entrez le nom de la réparation: ")
+        job_cost = float(input("Entrez le coût: ").replace("$",""))
+        job_length = float(input("Entrez la durée en heure: ").replace("h","").replace("heure", ""))
+        job_state = input("Entrez l'état actuel (fait, non-fait): ")
+        cars.append(Car(car_maker, car_model, car_year, car_color))
+        jobs.append(Repair(job_name, job_cost, job_length, job_state))
+
+    def remove_job(self):
+        self.display()
+        index = int(input("Entrez l'index: "))
+        self.cars.pop(index-1)
+        self.jobs.pop(index-1)
+
+    def done_job_hours(self):
+        print()
+        data = []
+        for job in self.jobs:
+            data.append(job)
+        print(data)
 
     def display(self):
+        print()
         i = 1
         for car, job in zip(self.cars, self.jobs):
             print(f"{i}. {car}: {job}")
-            i += 1
-            
+            i += 1           
 
     def menu(self):
         exit = False
         while not exit:
             user_sel = 0
             while not 1 <= user_sel <= 7:
+                print()
                 print("Menu des options: ")
-                print("1. ajouter/enlever une réparation")
-                print("2. voir liste des réparations")
-                print("3. voir total des heures de réparations faites")
+                print("1. Ajouter/Enlever une Réparation")
+                print("2. Voir Liste des Réparations")
+                print("3. Voir Total des Heures de Réparations Faites")
                 print("4. voir revenu total de réparations faites")
                 print("5. voir total des heures de réparations non-faites")
                 print("6. voir revenu total manqué de réparations non-faites.")
@@ -147,15 +81,24 @@ class Billing:
                     print("1. Ajouter une réparation à un véhicule")
                     print("2. Enlever une réparation à un véhicule")
                     op_sel = int(input("Choix: "))
-                self.add_job(self.bills)
+                if op_sel == 1:
+                    self.add_job()
+                else: 
+                    self.remove_job()
+            elif user_sel == 2:
+                self.display()
+            elif user_sel == 3:
+                self.done_job_hours()
+
+            
                 
 
 
 cars = []
-cars.append(Car("Nissan","Altima",2012,"Bleu"))
+cars.append(Car("Ford","Escape",2011,"Gris"))
 cars.append(Car("Ford","Taurus",2000,"Vert"))
 cars.append(Car("Honda","Civic",2020,"Noir"))
-cars.append(Car("Ford","Escape",2011,"Gris"))
+cars.append(Car("Nissan","Altima",2012,"Bleu"))
 cars.append(Car("Toyota","Corolla",2002,"Bleu"))
 
 jobs = []
@@ -166,7 +109,7 @@ jobs.append(Repair("Démarreur",800,5,"non-fait"))
 jobs.append(Repair("Pneus",80,1,"fait"))
 
 bills = Billing(cars, jobs)
-bills.display()
+bills.menu()
 
 
         
